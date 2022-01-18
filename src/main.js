86f47ed9cd010ed95f5a6a9a16eb492194ba850a
Ruovan <2454234258@qq.com>
2022-01-18 20:57:52 +0800

    docs📝:  更新文档和代码注释

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+// 以 Vue 项目中的 main.js 为例子（因为目前对Vue框架比较熟悉）
+// 当前这个 main.js 文件 就相当于 Vue 中的 main.js 文件
+// 在这里引入的所有东西，都会被打包到最终的 js 文件中
+// 样式也会打包到 js 文件中，只是在后面会通过 js 添加到 <style> 标签中
 import '../assets/index.css'
 
-console.log('hello webpack')
+console.log('====== hello webpack ======')
--- webpack.config.js
+++ webpack.config.js
@@ -9,33 +9,36 @@ console.log(123)
 module.exports = {
   // mode 用于设置开发模式
   mode: 'development',
-  // entry 用于设置打包的入口文件
-  entry: path.resolve(__dirname, './src/main.js'),
+  // entry 用于设置打包的入口文件，默认会去找 index.js 文件
+  entry: path.resolve(__dirname, 'src', 'main.js'),
   // output 用于设置打包后的输出文件
   output: {
-    filename: 'output.js', // 输出文件名
-    path: path.resolve(__dirname, './dist'), // 输出目录
+    // filename: 'output.js', // 输出文件名
+    // 事实上，输出文件名，我们一般用原来的文件名，再加上 hash值
+    // -- 而在 html 中也会自动引入这个文件
+    filename: '[name].[hash:8].js',
+    path: path.resolve(__dirname, 'dist'), // 输出目录
   },
-  // plugins 用于设置插件
+  // plugins 用于设置插件 -- 主要用来扩展 webpack 的功能
   plugins: [
+    // 自动在 HTML 中引入 JS
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, './public/index.html'),
     }),
-    new Webpack.HotModuleReplacementPlugin(),
   ],
-  // module 用于设置加载器
+  // module 用于设置加载器 -- 主要用来处理那些 webpack 不能识别的文件
   module: {
     rules: [
       {
-        test: /\.css$/, // 这里表示匹配所有 .css 文件
-        use: ['style-loader', 'css-loader'], // 这里表示使用 css-loader 解析 css 文件
+        test: /\.css$/, // 用正则匹配所有 .css 文件
+        use: ['style-loader', 'css-loader'], // 从后往前依次使用 Loader 去处理匹配的文件
       },
     ],
   },
   // 配置自动服务
   devServer: {
-    port: 9527,
-    hot: true,
-    open: true,
+    port: 9527, // 端口号
+    hot: true, // 热更新，需要配合 webpack 的热更新插件使用
+    open: true, // 自动打开浏览器
   },
 }
